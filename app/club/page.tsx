833752e3edffc5be99b2bc09e305a1fc7d66e6ff
JK <keki@JKs-Mac-mini-3.local>
2026-08-30 17:16:40 -0700

    Keep old app links working

---
+++ app/club/page.tsx
@@ -0,0 +1,5 @@
+import { ClubApp } from "@/components/ClubApp";
+
+export default function ClubAliasPage() {
+  return <ClubApp />;
+}
---
+++ app/parent/page.tsx
@@ -0,0 +1,5 @@
+import { ClubApp } from "@/components/ClubApp";
+
+export default function ParentAliasPage() {
+  return <ClubApp />;
+}
